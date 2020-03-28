14d7ae10022cf11e1ded04fa87a51fd949c429e1
Mukarram Mukhtar <mukarram@hotmail.com>
2020-03-28 13:08:53 -0500

    My first commit

---
+++ src/main/java/com/manidev/helloGitOne/HelloGit1Application.java
@@ -0,0 +1,14 @@
+package com.manidev.helloGitOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGit1Application.class, args);
+		System.out.println("Hello Git World!");
+	}
+
+}
---
+++ src/test/java/com/manidev/helloGitOne/HelloGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manidev.helloGitOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
